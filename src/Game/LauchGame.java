a6398fc3422ba8fecec6f0f4a578777c81d28640
Monisha027 <vampirevibes4@gmail.com>
2025-03-18 11:10:07 +0530

    Initial commit

---
+++ src/Game/LauchGame.java
@@ -0,0 +1,170 @@
+package Game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class TicTacToe{	
+	static char [][]board;
+	TicTacToe(){
+		board=new char[3][3];
+		initBoard();
+		
+	}	
+	void initBoard() {
+		for(int i=0;i<board.length;i++) {
+			  for(int j=0;j<board[i].length;j++) {
+				  board[i][j]=' ';
+			  }
+			}
+		}	
+	void displayBoard() {
+		System.out.println("----------");
+		for(int i=0;i<board.length;i++) {
+			  System.out.print("|");
+			  for(int j=0;j<board[i].length;j++) {
+				  System.out.print(board[i][j]+" |");
+			  }
+			  System.out.println();
+			  System.out.println("----------");
+			}	
+		}
+	static void placeMark(int row,int col,char mark) {
+		if(row>=0 && row<=2 && col>=0 && col<=2) {
+			board[row][col]=mark;
+		}
+		else {
+			System.out.println("Invalid position");
+		}
+	}
+	public boolean rowWin() {
+		for(int i=0;i<board.length;i++) {
+			if(board[i][0]!=' ' && board[i][0]==board[i][1] && board[i][1]==board[i][2]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean columnWin() {
+		for(int j=0;j<=2;j++) {
+			if(board[0][j]!=' ' && board[0][j]==board[1][j] && board[1][j]==board[2][j]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean diagonalWin() {
+			if(board[0][0]!=' ' && board[0][0]==board[1][1] && board[1][1]==board[2][2] || 
+					board[0][2]!=' ' && board[0][2]==board[1][1] && board[1][1]==board[2][0]) {
+				return true;
+			}
+		return false;
+	}
+	boolean checkDraw() {
+		for(int i=0;i<board.length;i++) {
+			for(int j=0;j<=2;j++) {
+				if(board[i][j]==' ') {
+					return false;
+				}	
+			}
+		}
+		return true;
+	}
+	
+	
+}
+abstract class Player{
+	String name;
+	char mark;
+	abstract void makeMove() ;
+	boolean isValid(int row,int col) {
+		if(row>=0 && row<=2 && col>=0 && col<=2) {
+			if(TicTacToe.board[row][col]==' ') {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
+class HumanPlayer extends Player{
+	
+	HumanPlayer(String name,char mark){
+		this.name=name;
+		this.mark=mark;
+	}
+	void makeMove() {
+		Scanner scan=new Scanner(System.in) ;
+		int row;
+		int column;
+		do {
+			System.out.println("enter row");
+		    row=scan.nextInt();	
+			System.out.println("enter column");
+			column=scan.nextInt();
+			
+		}while(!isValid(row,column));
+		TicTacToe.placeMark(row, column, mark);
+		
+	}
+	}
+class Ai extends Player{
+	Ai(String name,char mark){
+		this.name=name;
+		this.mark=mark;
+	}
+
+	@Override
+	void makeMove() {
+		// TODO Auto-generated method stub
+		Random r=new Random();
+		int row;
+		int column;
+		do {
+			System.out.println("enter row");
+		    row=r.nextInt(3);
+			System.out.println("enter column");
+			column=r.nextInt(3);
+			
+		}while(!isValid(row,column));
+		TicTacToe.placeMark(row, column, mark);
+	}
+	
+}
+
+public class LauchGame {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           TicTacToe t=new TicTacToe();
+           
+           HumanPlayer p1=new HumanPlayer("moni",'X');
+           HumanPlayer p2=new HumanPlayer("shree",'O');
+           Player cp=p1;
+           
+           while(true) {
+        	   System.out.println(cp.name+" turn");
+        	   cp.makeMove();
+        	   t.displayBoard();
+               if(t.columnWin()||t.rowWin()||t.diagonalWin()) {
+            	   System.out.println(cp.name +" wins");
+            	   break;
+               }
+               else if(t.checkDraw()) {
+            	   System.out.println("game is a draw");
+            	   break;
+               }
+               else {
+            	   
+            	
+            	   if(cp==p1) {
+            	       cp=p2;
+            	   }
+            	   else {
+            		   cp=p1;
+            	   }
+            	  
+               }
+           }
+           }
+	}
+
+
